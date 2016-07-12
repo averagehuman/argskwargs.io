e13de3c2fe5bb6f7b43c0b4f12ab13fd8a874142
averagehuman <cyclebelfast@gmail.com>
2016-07-12 20:49:49 +0100

    change gist plugin package

--- src/plugins/gist/__init__.py
+++ src/plugins/gist/__init__.py
@@ -1,9 +1,1 @@
-# -*- coding: utf-8 -*-
-__title__ = 'pelican-gist'
-__version__ = '0.3.2'
-__author__ = 'Chris Streeter'
-__license__ = 'MIT'
-__copyright__ = 'Copyright 2013'
-
-
-from pelican_gist.plugin import register
+from .gist import register
