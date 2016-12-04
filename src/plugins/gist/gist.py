950ec84586334e7b72cbdd26e3feb3172a86d7ef
averagehuman <cyclebelfast@gmail.com>
2016-12-04 18:37:25 +0000

    guard against gist plugin failure when passed a static file

--- src/plugins/gist/gist.py
+++ src/plugins/gist/gist.py
@@ -5,9 +5,10 @@ from pelican import signals
 
 def gist(content):
     """Converts [[ gist username:ID ]] in an article into its embedded <script> equivalent."""
-    pattern = r'\[\[ gist (\w+):(\w+) \]\]'
-    replacement = r'<script src="https://gist.github.com/\1/\2.js"></script>'
-    content._content = re.sub(pattern, replacement, content._content)
+    if content and content._content:
+        pattern = r'\[\[ gist (\w+):(\w+) \]\]'
+        replacement = r'<script src="https://gist.github.com/\1/\2.js"></script>'
+        content._content = re.sub(pattern, replacement, content._content)
 
 
 def register():
